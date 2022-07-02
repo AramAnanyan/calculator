41792236bf1e7d2c422589ad9505c256962092a9
Aram Ananyan <aramoananyan@gmail.com>
2022-07-02 11:16:44 +0400

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b, sum;
+            char x;
+            a = Convert.ToDouble(Console.ReadLine());
+            x = Convert.ToChar(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+
+            switch (x)
+            {
+                case '+':
+                    sum = a + b;
+                    Console.WriteLine(sum);
+                    break;
+                case '-':
+                    sum = a - b;
+                    Console.WriteLine(sum);
+                    break;
+                case '*':
+                    sum = a * b;
+                    Console.WriteLine(sum);
+                    break;
+                case '/':
+                    sum = a + b;
+                    Console.WriteLine(sum);
+                    break;
+                default:
+                    Console.WriteLine("wrong symbol "+ "'"+ x +"'");
+                    break;
+
+            }
+            
+        }
+    }
+}
